6a2319e857200cac7448fab450ee25fd96a9ba9c
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2020-07-18 11:24:18 -0600

    Analyze descriptive features of the dataset

--- src/analyze/descriptive.R
+++ src/analyze/descriptive.R
@@ -2,6 +2,91 @@
 library(tidyverse)
 
 # Import
+survey <- read_csv("data/covid_sex_tech.csv")
 
+# Drop other columns
+survey_1 <- survey[,c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18)]
 
-# CTRL SHIFT R ------------------------------------------------------------
+# Condense ethnicity
+survey_2 <- within(survey_1, {
+  ETHNIC <- recode(as.character(ETHNIC), "White Irish" = "White Other", 
+                   "White Black Caribbean" = "Mixed Ethnicity", "White Black African" = "Mixed Ethnicity", 
+                   "White Asian" = "Mixed Ethnicity", "Other Mixed" = "Mixed Ethnicity", 
+                   "Indian" = "South Asian", "Pakistani" = "South Asian", "Bangladeshi" = "South Asian", 
+                   "Chinese" = "Other Asian", "African" = "African / Carribean", 
+                   "Carribean" = "African / Carribean", "Other Black" = "Other Ethnicity",
+                   "Other Ethnic" = "Other Ethnicity")
+})
+
+# PARTICIPANT DEMOGRAPHICS ------------------------------------------------
+
+# Narrative descriptive statistics
+
+# Mean and SD of age
+survey_2 %>%
+  summarize(M = mean(AGE),
+            SD = sd(AGE)) %>%
+  as.data.frame()
+
+# Average health status
+survey_2 %>%
+  filter(HEALTH_STATUS <- !is.na(HEALTH_STATUS)) %>%
+  summarize(M = mean(HEALTH_STATUS),
+            SD = sd(HEALTH_STATUS)) %>%
+  as.data.frame()
+
+# Percent health status
+survey_2 %>%
+  group_by(HEALTH_STATUS) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
+
+# Percent self-isolate
+survey_2 %>%
+  group_by(SELF_ISOLATE) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
+
+# Percent key workers
+survey_2 %>%
+  group_by(KEY_WORKER) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
+
+# Tabulated descriptive statistics
+
+# Percent country
+survey_2 %>%
+  group_by(COUNTRY) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
+
+# Percent ethnicity
+survey_2 %>%
+  group_by(ETHNIC) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
+
+# Percent education
+survey_2 %>%
+  group_by(EDUCATION) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
+
+# Percent gender
+survey_2 %>%
+  group_by(GENDER, CISGENDER) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
+
+# Percent relationship status
+survey_2 %>%
+  group_by(RELATIONSHIP_STATUS) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
+
+# Percent current living
+survey_2 %>%
+  group_by(CURRENT_LIVING) %>%
+  summarize(count = n(),
+            percent = count / nrow(survey_2))
