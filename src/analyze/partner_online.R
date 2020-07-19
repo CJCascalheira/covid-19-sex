d57cc4f4435891dbb940222e64a08bb40bbbc4bf
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2020-07-19 10:46:52 -0600

    Start cleaning partner online variables

--- src/analyze/partner_online.R
+++ src/analyze/partner_online.R
@@ -6,5 +6,9 @@ survey <- read_csv("data/covid_sex_tech.csv")
 
 # CLEAN PARTNER ONLINE VARIABLES ------------------------------------------
 
+# Select primary variables
+partner_online <- survey %>%
+  select(starts_with(c("PARTNER", "ONLINE", "RELATIONSHIP"))) %>%
+  select(-ends_with(c("QUAL", "STATUS")))
 
 # DESCRIPTIVE ANALYSES ----------------------------------------------------
