02a71a49f622c13ef1dbcc488a9d594bd55d4383
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2020-07-19 18:35:13 -0600

    Clean sexual activities and sexual health variables

--- src/analyze/sexual_activities.R
+++ src/analyze/sexual_activities.R
@@ -6,4 +6,18 @@ survey <- read_csv("data/covid_sex_tech.csv")
 
 # CLEAN SEXUAL ACTIVITIES VARIABLES ---------------------------------------
 
+# Select the main variables
+sex_act <- survey %>%
+  select(starts_with("SA")) %>%
+  select(-ends_with("QUAL"))
+
 # DESCRIPTIVE ANALYSES ----------------------------------------------------
+
+# Have participants engaged in more sexual fantasizing during lockdown?
+within(sex_act, {
+  SA_STARTED_FANTASIZING <- recode(as.character(SA_STARTED_FANTASIZING), "0" = "No", "1" = "Yes")
+}) %>%
+  select(ends_with("ING")) %>%
+  group_by(SA_STARTED_FANTASIZING) %>%
+  count() %>%
+  mutate(percent = n / 565)
--- src/analyze/sexual_health.R
+++ src/analyze/sexual_health.R
@@ -6,4 +6,20 @@ survey <- read_csv("data/covid_sex_tech.csv")
 
 # CLEAN SEXUAL HEALTH VARIABLES -------------------------------------------
 
+# Select main variables
+sex_health <- survey %>%
+  select(starts_with("SEX_HEALTH")) %>%
+  select(-ends_with("QUAL"))
+
+# Label values across variables
+sex_health_label <- within(sex_health, {
+  SEX_HEALTH_VISIT <- recode(as.character(SEX_HEALTH_VISIT), "0" = "No", "1" = "Yes")
+  SEX_HEALTH_STISCRN <- recode(as.character(SEX_HEALTH_STISCRN), "0" = "Never", "1" = "Monthly",
+                               "2" = "Every 3 months", "3" = "Every 6 months", "4" = "Yearly",
+                               "5" = "Every few years")
+  SEX_HEALTH_STIGET <- recode(as.character(SEX_HEALTH_STIGET), "0" = "No", "1" = "Yes", "2" = "Unsure")
+  SEX_HEALTH_STISOCISO <- recode(as.character(SEX_HEALTH_STISOCISO), "0" = "No", "1" = "Yes")
+  SEX_HEALTH_HEALTHSOCISO <- recode(as.character(SEX_HEALTH_HEALTHSOCISO), "0" = "No", "1" = "Yes")
+})
+
 # DESCRIPTIVE ANALYSES ----------------------------------------------------
